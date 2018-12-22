38729f08a5af915372f65d612489093ce87a75cf
Jimenoza <luispama96@hotmail.com>
2018-12-21 22:45:25 -0600

    Ejemplo historial denuncia

--- rebasi/resources/views/record/index.blade.php
+++ rebasi/resources/views/record/index.blade.php
@@ -1,5 +1,45 @@
 @extends('layouts.complainant')
 
 @section('content')
-    hola
+<div class="content">
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-md-12">
+          <div class="card">
+            <div class="card-header card-header-primary">
+              <h4 class="card-title ">Historial de denuncias</h4>
+            </div>
+            <div class="card-body">
+              <div class="table-responsive">
+                <table class="table">
+                  <thead class=" text-primary">
+                    <th>ID</th>
+                    <th>Título</th>
+                    <th>Fecha</th>
+                    <th>Estado</th>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <td>
+                        1
+                      </td>
+                      <td>
+                        Basura en el río
+                      </td>
+                      <td>
+                        21-12-2018
+                      </td>
+                      <td class="text-primary">
+                        Iniciada
+                      </td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 @endsection
