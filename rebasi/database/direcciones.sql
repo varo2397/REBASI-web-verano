2a0189c5a32e6414120b1d193acced0c0ffbf140
Jimenoza <luispama96@hotmail.com>
2019-01-07 21:33:06 -0600

    provincias, cantones, distritos

---
+++ rebasi/database/direcciones.sql
@@ -0,0 +1,750 @@
+insert into province(provinceID,name)
+values (1,'San José'),
+(2,'Alajuela'),
+(3,'Cartago'),
+(4,'Heredia'),
+(5,'Guanacaste'),
+(6,'Puntarenas'),
+(7,'Limón');
+
+insert into canton(cantonID,province,name)
+values(1,1,'San José'),
+(2,1,'Escazú'),
+(3,1,'Desamparados'),
+(4,1,'Puriscal'),
+(5,1,'Tarrazú'),
+(6,1,'Aserrí'),
+(7,1,'Mora'),
+(8,1,'Goicoechea'),
+(9,1,'Santa Ana'),
+(10,1,'Alajuelita'),
+(11,1,'Vázques de Coronado'),
+(12,1,'Acosta'),
+(13,1,'Tibás'),
+(14,1,'Moravia'),
+(15,1,'Montes de Oca'),
+(16,1,'Turrubares'),
+(17,1,'Dota'),
+(18,1,'Curridabat'),
+(19,1,'Pérez Zeledón'),
+(20,1,'León Cortés');
+
+
+insert into district(districtID,canton,name)
+values(1,1,'Carmen'),
+(2,1,'Merced'),
+(3,1,'Hospital'),
+(4,1,'Catedral'),
+(5,1,'Zapote'),
+(6,1,'San Francisco de Dos Ríos'),
+(7,1,'La Uruca'),
+(8,1,'Mata redonda'),
+(9,1,'Pavas'),
+(10,1,'Hatillo'),
+(11,1,'San Sebastián');
+
+insert into district(districtID,canton,name)
+values(12,2,'Escazú Centro'),
+(13,2,'San Rafael'),
+(14,2,'San Antonio');
+
+insert into district(districtID,canton,name)
+values(15,3,'Desamparados'),
+(16,3,'San Miguel'),
+(17,3,'San Juan de Dios'),
+(18,3,'San Rafael Arriba'),
+(19,3,'San Antonio'),
+(20,3,'Frailes'),
+(21,3,'Patarrá'),
+(22,3,'San Cristóbal'),
+(23,3,'Rosario'),
+(24,3,'Damas'),
+(25,3,'San Rafael Abajo'),
+(26,3,'Gravilias'),
+(27,3,'Los Guido');
+
+insert into district(districtID,canton,name)
+values(28,4,'Santiago'),
+(29,4,'Mercedes Sur'),
+(30,4,'Barbacoas'),
+(31,4,'Grifo Alto'),
+(32,4,'San Rafael'),
+(33,4,'Candelarita'),
+(34,4,'Desamparaditos'),
+(35,4,'San Antonio'),
+(36,4,'Chires'),
+(37,4,'La Cangreja');
+
+insert into district(districtID,canton,name)
+values(38,5,'San Marcos'),
+(39,5,'San Lorenzo'),
+(40,5,'San Carlos');
+
+insert into district(districtID,canton,name)
+values(41,6,'Aserrí'),
+(42,6,'Tarbaca'),
+(43,6,'Vuelta de Jorco'),
+(44,6,'San Gabriel'),
+(45,6,'Legua'),
+(46,6,'Monterrey'),
+(47,6,'Salitrillos');
+
+insert into district(districtID,canton,name)
+values(48,7,'Colón'),
+(49,7,'Guayabo'),
+(50,7,'Tabarcia'),
+(51,7,'Piedras Negras'),
+(52,7,'Picagres'),
+(53,7,'Jaris'),
+(54,7,'Quitirrisí');
+
+insert into district(districtID,canton,name)
+values(55,8,'Guadalupe'),
+(56,8,'San Francisco'),
+(57,8,'Calle Blancos'),
+(58,8,'Mata de Plátano'),
+(59,8,'Ipís'),
+(60,8,'Rancho Redondo'),
+(61,8,'Purral');
+
+insert into district(districtID,canton,name)
+values(62,9,'Santa Ana'),
+(63,9,'Salitral'),
+(64,9,'Pozos'),
+(65,9,'Uruca'),
+(66,9,'Piedades'),
+(67,9,'Brasil');
+
+insert into district(districtID,canton,name)
+values(68,10,'Alajuelita Centro'),
+(69,10,'San Josecito'),
+(70,10,'San Antonio'),
+(71,10,'Concepción'),
+(72,10,'San Felipe');
+
+insert into district(districtID,canton,name)
+values(73,11,'San Isidro'),
+(74,11,'San Rafael'),
+(75,11,'Dulce Nombre de Jesús'),
+(76,11,'Patalillo'),
+(77,11,'Cascajal');
+
+insert into district(districtID,canton,name)
+values(78,12,'San Ignacio'),
+(79,12,'Guaitíl'),
+(80,12,'Palmichal'),
+(81,12,'Cangrejal'),
+(82,12,'Sabanillas');
+
+insert into district(districtID,canton,name)
+values(83,13,'San Juan'),
+(84,13,'Cinco Esquinas'),
+(85,13,'Anselmo Llorente'),
+(86,13,'Leon XIII'),
+(87,13,'Colima');
+
+insert into district(districtID,canton,name)
+values(88,14,'San Vicente'),
+(89,14,'San Jerónimo'),
+(90,14,'La Trinidad');
+
+insert into district(districtID,canton,name)
+values(91,15,'San Pedro'),
+(92,15,'Sabanilla'),
+(93,15,'Mercedes'),
+(94,15,'San Rafael');
+
+insert into district(districtID,canton,name)
+values(95,16,'San Pablo'),
+(96,16,'San Pedro'),
+(97,16,'San Juan de Mata'),
+(98,16,'San Luis'),
+(99,16,'Carara');
+
+insert into district(districtID,canton,name)
+values(100,17,'Santa María'),
+(101,17,'Jardín'),
+(102,17,'Copey');
+
+insert into district(districtID,canton,name)
+values(103,18,'Curridabat'),
+(104,18,'Granadilla'),
+(105,18,'Sánchez'),
+(106,18,'Tirrases');
+
+insert into district(districtID,canton,name)
+values(107,19,'San Isidro del General'),
+(108,19,'El General'),
+(109,19,'Daniel Flores'),
+(110,19,'Rivas'),
+(111,19,'San Pedro'),
+(112,19,'Platanares'),
+(113,19,'Pejibaye'),
+(114,19,'Barú'),
+(115,19,'Río Nuevo'),
+(116,19,'Páramo'),
+(117,19,'La Amistad'),
+(118,19,'Cajón');
+
+insert into district(districtID,canton,name)
+values(119,20,'San Pablo'),
+(120,20,'San Andrés'),
+(121,20,'Llano bonito'),
+(122,20,'San Isidro'),
+(123,20,'Santa Cruz'),
+(124,20,'San Antonio');
+
+insert into canton(cantonID,province,name)
+values(21,2,'Alajuela'),
+(22,2,'San Ramón'),
+(23,2,'Grecia'),
+(24,2,'San Mateo'),
+(25,2,'Atenas'),
+(26,2,'Naranjo'),
+(27,2,'Palmares'),
+(28,2,'Poás'),
+(29,2,'Orotina'),
+(30,2,'San Carlos'),
+(31,2,'Zarcero'),
+(32,2,'Valverde Vega'),
+(33,2,'Upala'),
+(34,2,'Los Chiles'),
+(35,2,'Guatuso');
+
+insert into district(districtID,canton,name)
+values(125,21,'Alajuela'),
+(126,21,'San José'),
+(127,21,'Carrizal'),
+(128,21,'San Antonio'),
+(129,21,'Guácima'),
+(130,21,'San Isidro'),
+(131,21,'Sabanilla'),
+(132,21,'San Rafael'),
+(133,21,'Río Segundo'),
+(134,21,'Desamparados'),
+(135,21,'Turrúcares'),
+(136,21,'Tambor'),
+(137,21,'Garita'),
+(138,21,'Sarapiquí');
+
+insert into district(districtID,canton,name)
+values(139,22,'San Ramón'),
+(140,22,'Santiago'),
+(141,22,'San Juan'),
+(142,22,'Piedades Norte'),
+(143,22,'Piedades Sur'),
+(144,22,'San Rafael'),
+(145,22,'San Isidro'),
+(146,22,'Ángeles'),
+(147,22,'Alfaro'),
+(148,22,'Volio'),
+(149,22,'Concepción'),
+(150,22,'Zapotal'),
+(151,22,'Peñas Blancas');
+
+insert into district(districtID,canton,name)
+values(152,23,'Grecia'),
+(153,23,'San Isidro'),
+(154,23,'San José'),
+(155,23,'San Roque'),
+(156,23,'Tacares'),
+(157,23,'Río Cuarto'),
+(158,23,'Puente de Piedra'),
+(159,23,'Bolívar');
+
+insert into district(districtID,canton,name)
+values(160,24,'San Mateo'),
+(161,24,'Desmonte'),
+(162,24,'Jesús María'),
+(163,24,'Labrador');
+
+insert into district(districtID,canton,name)
+values(164,25,'Atenas'),
+(165,25,'Jesús'),
+(166,25,'Mercedes'),
+(167,25,'San Isidro'),
+(168,25,'Concepción de Río Grande'),
+(169,25,'San José'),
+(170,25,'Santa Eulalia'),
+(171,25,'Escobal');
+
+insert into district(districtID,canton,name)
+values(172,26,'Naranjo'),
+(173,26,'San Miguel'),
+(174,26,'San José'),
+(175,26,'Cirrí Sur'),
+(176,26,'San Jerónimo'),
+(177,26,'San Juan'),
+(178,26,'Rosario');
+
+insert into district(districtID,canton,name)
+values(179,27,'Palmares'),
+(180,27,'Zaragoza'),
+(181,27,'Buenos Aires'),
+(182,27,'Santiago'),
+(183,27,'Candelaria'),
+(184,27,'Esquipulas'),
+(185,27,'La Granja');
+
+insert into district(districtID,canton,name)
+values(186,28,'San Pedro'),
+(187,28,'San Juan'),
+(188,28,'San Rafael'),
+(189,28,'Carrillos'),
+(190,28,'Sabana Redonda');
+
+insert into district(districtID,canton,name)
+values(191,29,'Orotina'),
+(192,29,'El Mastate'),
+(193,29,'Hacienda Vieja'),
+(194,29,'Coyolar'),
+(195,29,'La Ceiba');
+
+insert into district(districtID,canton,name)
+values(196,30,'Quesada'),
+(197,30,'Florencia'),
+(198,30,'Buena Vista'),
+(199,30,'Aguas Zarcas'),
+(200,30,'Venecia'),
+(201,30,'Pital'),
+(202,30,'La Fortuna'),
+(203,30,'La Tigra'),
+(204,30,'Palmera'),
+(205,30,'Venado'),
+(206,30,'Cutris'),
+(207,30,'Monterrey'),
+(208,30,'Pocosol');
+
+insert into district(districtID,canton,name)
+values(209,31,'Zarcero'),
+(210,31,'Laguna'),
+(211,31,'Tapezco'),
+(212,31,'Guadalupe'),
+(213,31,'Palmira'),
+(214,31,'Zapote'),
+(215,31,'Brisas');
+
+insert into district(districtID,canton,name)
+values(216,32,'Sarchí Norte'),
+(217,32,'Sarchí Sur'),
+(218,32,'Bajos del Toro'),
+(219,32,'San Pedro'),
+(220,32,'San Juan');
+
+insert into district(districtID,canton,name)
+values(221,33,'Upala'),
+(222,33,'Aguas Claras'),
+(223,33,'San José'),
+(224,33,'Bijagua'),
+(225,33,'Delicias'),
+(226,33,'Dos Rios'),
+(227,33,'Yolillal'),
+(228,33,'Canalete');
+
+insert into district(districtID,canton,name)
+values(229,34,'Los Chiles'),
+(230,34,'Caño Negro'),
+(231,34,'El Amparo'),
+(232,34,'San Jorge');
+
+insert into district(districtID,canton,name)
+values(233,35,'San Rafael'),
+(234,35,'Buena Vista'),
+(235,35,'Cote'),
+(236,35,'Katira');
+
+insert into canton(cantonID,province,name)
+values(36,3,'Cartago'),
+(37,3,'Paraíso'),
+(38,3,'La Unión'),
+(39,3,'Jiménez'),
+(40,3,'Turrialba'),
+(41,3,'Alvarado'),
+(42,3,'Oreamuno'),
+(43,3,'El Guarco');
+
+insert into district(districtID,canton,name)
+values(237,36,'Oriental'),
+(238,36,'Occidental'),
+(239,36,'Carmen'),
+(240,36,'Taras'),
+(241,36,'Agua Caliente'),
+(242,36,'Guadalupe'),
+(243,36,'Corralillo'),
+(244,36,'Tierra Blanca'),
+(245,36,'Dulce Nombre'),
+(246,36,'Llano Grande'),
+(247,36,'Quebradillo');
+
+insert into district(districtID,canton,name)
+values(248,37,'Paraíso'),
+(249,37,'Orosi'),
+(250,37,'Cachí'),
+(251,37,'Santiago'),
+(252,37,'Llanos de Santa Lucía');
+
+insert into district(districtID,canton,name)
+values(253,38,'Tres Ríos'),
+(254,38,'San Diego'),
+(255,38,'San Juan'),
+(256,38,'San Rafael'),
+(257,38,'Concepción'),
+(258,38,'Dulce Nombre'),
+(259,38,'San Ramón'),
+(260,38,'Río Azul');
+
+insert into district(districtID,canton,name)
+values(261,39,'Juan Viñas'),
+(262,39,'Tucurrique'),
+(263,39,'Pejibaye');
+
+insert into district(districtID,canton,name)
+values(264,40,'Turrialba'),
+(265,40,'La Suiza'),
+(266,40,'Peralta'),
+(267,40,'Santa Cruz'),
+(268,40,'Santa Teresita'),
+(269,40,'Pavones'),
+(270,40,'Tuis'),
+(271,40,'Tayutic'),
+(272,40,'Santa Rosa'),
+(273,40,'Tres Equis'),
+(274,40,'La Isabel'),
+(275,40,'Chirripó');
+
+insert into district(districtID,canton,name)
+values(276,41,'Pacayas'),
+(277,41,'Cervantes'),
+(278,41,'Capellades');
+
+insert into district(districtID,canton,name)
+values(279,42,'San Rafael'),
+(280,42,'Cot'),
+(281,42,'Potrero Cerrado'),
+(282,42,'Cipreses'),
+(283,42,'Santa Rosa');
+
+insert into district(districtID,canton,name)
+values(284,43,'El Tejar'),
+(285,43,'San Isidro'),
+(286,43,'Tobosi'),
+(287,43,'Patio de Agua');
+
+insert into canton(cantonID,province,name)
+values(44,4,'Heredia'),
+(45,4,'Barva'),
+(46,4,'Santo Domingo'),
+(47,4,'Santa Bárbara'),
+(48,4,'San Rafael'),
+(49,4,'San Isidro'),
+(50,4,'Belén'),
+(51,4,'Flores'),
+(52,4,'San Pablo'),
+(53,4,'Sarapiquí');
+
+insert into district(districtID,canton,name)
+values(288,44,'Heredia'),
+(289,44,'Mercedes'),
+(290,44,'San Francisco'),
+(291,44,'Ulloa'),
+(292,44,'Vara Blanca');
+
+insert into district(districtID,canton,name)
+values(293,45,'Barva'),
+(294,45,'San Pedro'),
+(295,45,'San Pablo'),
+(296,45,'San Roque'),
+(297,45,'Santa Lucía'),
+(298,45,'San José de la Montaña');
+
+insert into district(districtID,canton,name)
+values(299,46,'Santo Domingo'),
+(300,46,'San Vicente'),
+(301,46,'San Miguel'),
+(302,46,'Paracito'),
+(303,46,'Santo Tomás'),
+(304,46,'Santa Rosa'),
+(305,46,'Tures'),
+(306,46,'Pará');
+
+insert into district(districtID,canton,name)
+values(307,47,'Santa Bárbara'),
+(308,47,'San Pedro'),
+(309,47,'San Juan'),
+(310,47,'Jesús'),
+(311,47,'Santo Domingo'),
+(312,47,'Purabá');
+
+insert into district(districtID,canton,name)
+values(313,48,'San Rafael'),
+(314,48,'San Josecito'),
+(315,48,'Santiago'),
+(316,48,'Los Ángeles'),
+(317,48,'Concepción');
+
+insert into district(districtID,canton,name)
+values(318,49,'San Isidro'),
+(319,49,'San José'),
+(320,49,'Concepción'),
+(321,49,'San Francisco');
+
+insert into district(districtID,canton,name)
+values(322,50,'San Antonio'),
+(323,50,'La Rivera'),
+(324,50,'La Asunción');
+
+insert into district(districtID,canton,name)
+values(325,51,'San Joaquín'),
+(326,51,'Barrantes'),
+(327,51,'Llorente');
+
+insert into district(districtID,canton,name)
+values(328,52,'San Pablo'),
+(329,52,'Rincón de Sabanilla');
+
+insert into district(districtID,canton,name)
+values(330,53,'Puerto Viejo'),
+(331,53,'La Virgen'),
+(332,53,'Horquetas'),
+(333,53,'Llanuras del Gaspar'),
+(334,53,'Cureña');
+
+insert into canton(cantonID,province,name)
+values(54,5,'Liberia'),
+(55,5,'Nicoya'),
+(56,5,'Santa Cruz'),
+(57,5,'Bagaces'),
+(58,5,'Carrillo'),
+(59,5,'Cañas'),
+(60,5,'Abangares'),
+(61,5,'Tilarán'),
+(62,5,'Nandayure'),
+(63,5,'La Cruz'),
+(64,5,'Hojancha');
+
+insert into district(districtID,canton,name)
+values(335,54,'Liberia'),
+(336,54,'Mayorga'),
+(337,54,'Nacascolo'),
+(338,54,'Curubandé'),
+(339,54,'Cañas Dulces');
+
+insert into district(districtID,canton,name)
+values(340,55,'Nicoya'),
+(341,55,'Mansión'),
+(342,55,'San Antonio'),
+(343,55,'Quebrada Honda'),
+(344,55,'Sámara'),
+(345,55,'Nosara'),
+(346,55,'Belén');
+
+insert into district(districtID,canton,name)
+values(347,56,'Santa Cruz'),
+(348,56,'Bolsón'),
+(349,56,'Veinticiete de Abril'),
+(350,56,'Tempate'),
+(351,56,'Cartagena'),
+(352,56,'Cuajiniquil'),
+(353,56,'Diriá'),
+(354,56,'Cabo Velas'),
+(355,56,'Tamarindo');
+
+insert into district(districtID,canton,name)
+values(356,57,'Bagaces'),
+(357,57,'La Fortuna'),
+(358,57,'Mogote'),
+(359,57,'Río Naranjo');
+
+insert into district(districtID,canton,name)
+values(360,58,'Filadelfia'),
+(361,58,'Palmira'),
+(362,58,'Sardinal'),
+(363,58,'Belén');
+
+insert into district(districtID,canton,name)
+values(364,59,'Cañas'),
+(365,59,'Palmira'),
+(366,59,'San Miguel'),
+(367,59,'Bebedero'),
+(368,59,'Porozal');
+
+insert into district(districtID,canton,name)
+values(369,60,'Las Juntas'),
+(370,60,'Cierra'),
+(371,60,'San Juan'),
+(372,60,'Colorado');
+
+insert into district(districtID,canton,name)
+values(373,61,'Tilarán'),
+(374,61,'Quebrada Grande'),
+(375,61,'Tronadora'),
+(376,61,'Santa Rosa'),
+(377,61,'Líbano'),
+(378,61,'Tierras Morenas'),
+(379,61,'Arenal');
+
+insert into district(districtID,canton,name)
+values(380,62,'Carmona'),
+(381,62,'Santa Rita'),
+(382,62,'Zapotal'),
+(383,62,'San Pablo'),
+(384,62,'Provenir'),
+(385,62,'Bejuco');
+
+insert into district(districtID,canton,name)
+values(386,63,'La Cruz'),
+(387,63,'Santa Cecilia'),
+(388,63,'La Garita'),
+(389,63,'Santa Elena');
+
+insert into district(districtID,canton,name)
+values(390,64,'Hojancha'),
+(391,64,'Monte Romo'),
+(392,64,'Puerto Carrillo'),
+(393,64,'Haucas'),
+(394,64,'Matambú');
+
+insert into canton(cantonID,province,name)
+values(65,6,'Puntarenas'),
+(66,6,'Esparza'),
+(67,6,'Buenos Aires'),
+(68,6,'Montes de Oro'),
+(69,6,'Osa'),
+(70,6,'Quepos'),
+(71,6,'Golfito'),
+(72,6,'Coto Brus'),
+(73,6,'Parrita'),
+(74,6,'Corredores'),
+(75,6,'Garabito');
+
+insert into district(districtID,canton,name)
+values(395,65,'Puntarenas'),
+(396,65,'Pitahaya'),
+(397,65,'Chomes'),
+(398,65,'Lepanto'),
+(399,65,'Paquera'),
+(400,65,'Manzanillo'),
+(401,65,'Guacimal'),
+(402,65,'Barranca'),
+(403,65,'Monteverde'),
+(404,65,'Isla del Coco'),
+(405,65,'Cóbano'),
+(406,65,'Chacarita'),
+(407,65,'Chira'),
+(408,65,'Acapulco'),
+(409,65,'El Roble'),
+(410,65,'Arancibia');
+
+insert into district(districtID,canton,name)
+values(411,66,'Espíritu Santo'),
+(412,66,'San Juan'),
+(413,66,'Macacona'),
+(414,66,'San Rafael'),
+(415,66,'San Jerónimo'),
+(416,66,'Caldera');
+
+insert into district(districtID,canton,name)
+values(417,67,'Buenos Aires'),
+(418,67,'Volcán'),
+(419,67,'Potrero Grande'),
+(420,67,'Boruca'),
+(421,67,'Pilas'),
+(422,67,'Colinas'),
+(423,67,'Chánguena'),
+(424,67,'Biolley'),
+(425,67,'Brunka');
+
+insert into district(districtID,canton,name)
+values(426,68,'Miramar'),
+(427,68,'La Unión'),
+(428,68,'San Isidro');
+
+insert into district(districtID,canton,name)
+values(429,69,'Ciudad Cortés'),
+(430,69,'Palmar'),
+(431,69,'Sierpe'),
+(432,69,'Piedras Blancas'),
+(433,69,'Bahía Ballena'),
+(434,69,'Bahía Drake');
+
+insert into district(districtID,canton,name)
+values(435,70,'Quepos'),
+(436,70,'Savegre'),
+(437,70,'Naranjito');
+
+insert into district(districtID,canton,name)
+values(438,71,'Golfito'),
+(439,71,'Puerto Jiménez'),
+(440,71,'Guaycará'),
+(441,71,'Pavón');
+
+insert into district(districtID,canton,name)
+values(442,72,'San Vito'),
+(443,72,'Sabalito'),
+(444,72,'Agua Buena'),
+(445,72,'Limoncito'),
+(446,72,'Pittier'),
+(447,72,'Gutiérrez Brown');
+
+insert into district(districtID,canton,name)
+values(448,73,'Parrita');
+
+insert into district(districtID,canton,name)
+values(449,74,'Corredor'),
+(450,74,'La Cuesta'),
+(451,74,'Canoas'),
+(452,74,'Laurel');
+
+insert into district(districtID,canton,name)
+values(453,75,'Jacó'),
+(454,75,'Tárcoles');
+
+insert into canton(cantonID,province,name)
+values(76,7,'Limón'),
+(77,7,'Pococí'),
+(78,7,'Siquirres'),
+(79,7,'Talamanca'),
+(80,7,'Matina'),
+(81,7,'Guácimo');
+
+insert into district(districtID,canton,name)
+values(455,76,'Limón'),
+(456,76,'Valle La Estrella'),
+(457,76,'Río Blanco'),
+(458,76,'Matama');
+
+insert into district(districtID,canton,name)
+values(459,77,'Guápiles'),
+(460,77,'Jiménez'),
+(461,77,'La Rita'),
+(462,77,'Roxana'),
+(463,77,'Cariari'),
+(464,77,'Colorado'),
+(465,77,'La Colonia');
+
+insert into district(districtID,canton,name)
+values(466,78,'Siquirres'),
+(467,78,'Pacuarito'),
+(468,78,'Florida'),
+(469,78,'Germania'),
+(470,78,'Cairo'),
+(471,78,'Alegría');
+
+insert into district(districtID,canton,name)
+values(472,79,'Bratsi'),
+(473,79,'Sixaola'),
+(474,79,'Cahuita'),
+(475,79,'Telire');
+
+insert into district(districtID,canton,name)
+values(476,80,'Matina'),
+(477,80,'Batán'),
+(478,80,'Carrandí');
+
+insert into district(districtID,canton,name)
+values(479,81,'Guácimo'),
+(480,81,'Mercedes'),
+(481,81,'Pocora'),
+(482,81,'Río Jiménez'),
+(483,81,'Duacarí');
+
